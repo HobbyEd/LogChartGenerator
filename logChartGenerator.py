5e7444b9d0624e3bd2145221e0f0c19d71b5cdc4
HobbyEd <edwinvandillen@gmail.com>
2018-10-11 18:39:12 +0200

    Bugfixe: when a log file is not JSON valide it will be skipped

--- logChartGenerator.py
+++ logChartGenerator.py
@@ -7,34 +7,39 @@ import io
 import time
 import sys
 
-#Import chart libaries
+# Import chart libaries
 import plotly
 import plotly.plotly as plotlyOnline
 import plotly.graph_objs as plotlyChart
 
-class LogChartPlotter: 
-    logSeries = [] #contains all the log data that is aviable on disk
-   
 
-    def __init__(self ):
-        #first read in all the logFiles
+class LogChartPlotter: 
+    logSeries = []  # contains all the log data that is aviable on disk
+   
+    def __init__(self):
+        # first read in all the logFiles
         self.logSeries = self.loadLogFiles()
 
     # This function read the logfiles in the folder /Logs and return a dictionary with the JSON data
     def loadLogFiles(self):
         folderPath = "./logs"
         data = []
-        #read all t he logfiles in the directory
+        # read all t he logfiles in the directory
         for filename in glob.glob(os.path.join(folderPath, '*.txt')):
             with io.open(filename, encoding='utf-16') as f:
-                fileTimeStamp = os.path.getmtime(filename)
-                buffer = json.load(f)
-                for logEntry in buffer: 
-                    logEntry['timeStamp'] = fileTimeStamp
-                data.append(buffer)
-                f.close() 
+                try:
+                    fileTimeStamp = os.path.getmtime(filename)
+                    buffer = json.load(f)
+                    for logEntry in buffer: 
+                        logEntry['timeStamp'] = fileTimeStamp
+                    data.append(buffer)
+                except Exception as e: 
+                    print(str(e) + " " + filename) 
+                finally:
+                    f.close() 
+     
 
-        #write a temperory logfile 
+        # write a temperory logfile 
         with io.open("log.txt", 'w', encoding='utf-16') as f:
             json.dump(str(data), f)
             f.close()
@@ -54,9 +59,9 @@ class LogChartPlotter:
         traces = [] #A trace is a timeseries that will be plotted on the charged. 
         traces.append(self.generateTraceForPlotly("LRV 2017", "6d9b225a-6694-42a6-9797-6f690cbc00d0"))
         traces.append(self.generateTraceForPlotly("TVU-Hoofdrol 2017", "694ddfe7-597d-49f3-b188-e3fed692ba32"))
-        traces.append(self.generateTraceForPlotly("LRV 2017 (2)", "7232aee2-d650-4dba-b34e-82f00d552745"))
         traces.append(self.generateTraceForPlotly("TKV 2017", "678ddd79-87ca-4b9e-bc10-f7f41e7a0b13"))
         traces.append(self.generateTraceForPlotly("Beeld & Geluid 2017", "3ba41222-0823-4273-84c2-e5197f127636"))
+        traces.append(self.generateTraceForPlotly("TVU-Hoofdrol 2015", "7232aee2-d650-4dba-b34e-82f00d552745"))
         traces.append(self.generateTraceForPlotly("Kleine geldstr. speech 2017", "a8737169-b5e6-497e-b94a-9b7b61526aa1"))
         traces.append(self.generateTraceForPlotly("TKV 2016", "0973ba5d-9f16-4341-8f2d-27db2ab3d3ed"))
         traces.append(self.generateTraceForPlotly("LRV 2016", "870c41bd-bf75-441a-96f6-4a4b2fda8083"))
@@ -108,4 +113,3 @@ if __name__ == "__main__":
         plotter.generateChartPlotly(True)
     else: 
         print (" +++ Only the parameter 'online' is accepted. By default the chart is plotted offline")
-
